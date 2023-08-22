a5e500675c5f0ee44802771c87507b1b75edcc81
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2023-08-21 23:37:27 -0400

    started project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Chris Orellana on 8/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            ZStack {
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
+                    HStack {
+                        Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("California")
+                            .font(.subheadline)
+                    }
+                    
+                }
+                .padding()
+                    
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # landmarks-ui-app
 Following the tutorial from Apple's dev site: https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
+
+
+## Some important concepts
+
+- Swift UI uses the `App` protocol
+- Clicking + Command Key on an item can give a special context menu
+![image1](images/image1.png)
+- The Assets.xcassets is where you can import resources like images for your project
+![image2](images/image2.png)
